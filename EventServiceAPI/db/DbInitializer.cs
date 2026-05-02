cef1ca33075a35bbb66c55f48ab95cea4ce4edcc
Ötvös Márton Álmos <otvos.marton.almos@gmail.com>
2026-05-02 22:06:23 +0200

    fix db initializer path, fix Dockerfile paths

--- EventServiceAPI/db/DbInitializer.cs
+++ EventServiceAPI/db/DbInitializer.cs
@@ -18,7 +18,7 @@ public class DbInitializer
         await using var conn = new NpgsqlConnection(_connectionString);
         await conn.OpenAsync();
 
-        var path = Path.Combine(AppContext.BaseDirectory, "schema", "events.sql");
+        var path = Path.Combine(AppContext.BaseDirectory, "db", "events.sql");
         var sql = await File.ReadAllTextAsync(path);
 
         var cmd = new NpgsqlCommand(sql, conn);
